cef4e0307f9b1279acc995a2d63580a81461ebde
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-01-10 19:26:50 +0545

    add emailjs, react-hook-form and react-toastify package install.

--- src/components/contact/contact-info/index.jsx
+++ src/components/contact/contact-info/index.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./style.scss";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const ContactInfo = () => {
-  return <div>ContactInfo</div>;
+  return (
+    <div className="contact-info-box">
+      <h4>Contact Info</h4>
+      <p>
+        I would be happy to answer any questions you may have about freelance
+        writing or content marketing!
+      </p>
+      <div className="contact-option">
+        <FaPhoneAlt />
+        <span className="text">+977 9824367788</span>
+      </div>
+      <div className="contact-option">
+        <MdEmail />
+        <span className="text">chaudharypradip678@gmail.com</span>
+      </div>
+    </div>
+  );
 };
 
 export default ContactInfo;
