966f2dac4e154f47689a404a9c5e7097aa8773ab
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-03-24 05:39:56 +0545

    finx intro component fix design issue.

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,41 +1,58 @@
 import React from "react";
 import {
-  FaYoutube,
-  FaGithub,
-  FaLinkedinIn,
-  FaStackOverflow,
-  FaInstagram,
+	FaYoutube,
+	FaGithub,
+	FaLinkedinIn,
+	FaStackOverflow,
+	FaInstagram,
 } from "react-icons/fa";
 import Section from "../shared/section";
 import Logo from "../../images/logo.png";
 import SocialIcon from "./social-icon";
 const Footer = () => {
-  return (
-    <Section background="dark" className="footer">
-      <div className="footer-contain-wrapper">
-        <div className="footer-logo">
-          <img src={Logo} alt="Pradip Chaudhary" />
-        </div>
-        <ul className="footer-menu-item"></ul>
-        <div className="social-icons">
-          <SocialIcon
-            color="#FF0000"
-            icon={<FaYoutube />}
-            link="https://www.youtube.com"
-          />
-          <SocialIcon color="#0D2636" icon={<FaGithub />} />
-          <SocialIcon color="#0A66C2" icon={<FaLinkedinIn />} />
-          <SocialIcon color="#f2740d" icon={<FaStackOverflow />} />
-          <SocialIcon color="#E84C88" icon={<FaInstagram />} />
-        </div>
-        <div className="bottom-bar">
-          <div className="copyright-text">
-            Copyright 2022 JS Dev Hindi | All Rights Reserved
-          </div>
-        </div>
-      </div>
-    </Section>
-  );
+	return (
+		<Section
+			background='dark'
+			className='footer'>
+			<div className='footer-contain-wrapper'>
+				<div className='footer-logo'>
+					<img
+						src={Logo}
+						alt='Pradip Chaudhary'
+					/>
+				</div>
+				<ul className='footer-menu-item'></ul>
+				<div className='social-icons'>
+					<SocialIcon
+						color='#FF0000'
+						icon={<FaYoutube />}
+						link='https://www.youtube.com'
+					/>
+					<SocialIcon
+						color='#0D2636'
+						icon={<FaGithub />}
+					/>
+					<SocialIcon
+						color='#0A66C2'
+						icon={<FaLinkedinIn />}
+					/>
+					<SocialIcon
+						color='#f2740d'
+						icon={<FaStackOverflow />}
+					/>
+					<SocialIcon
+						color='#E84C88'
+						icon={<FaInstagram />}
+					/>
+				</div>
+				<div className='bottom-bar'>
+					<div className='copyright-text'>
+						Copyright 2022 JS Dev Hindi | All Rights Reserved.
+					</div>
+				</div>
+			</div>
+		</Section>
+	);
 };
 
 export default Footer;
--- src/components/intro/intro-content/index.jsx
+++ src/components/intro/intro-content/index.jsx
@@ -6,44 +6,48 @@ import { BsAwardFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
 
 const IntroContent = () => {
-  return (
-    <div className="intro-content">
-      <div className="layout">
-        <div className="left-col">
-          <h1 className="title">
-            <span className="small-text">
-              <span className="text">Hello ✋, I am </span>
-            </span>
-            <span className="big-text">Pradip Chaudhary</span>
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-            placeat fugit ratione reprehenderit minus cum dicta id vero quos?
-            Cumque nemo cupiditate omnis voluptas alias!
-          </p>
-          <CallToAction text="Contact me" />
-        </div>
-        <div className="right-col">
-          <img src={girl} alt="Girlimage" />
-          <div className="highlights horizontal">
-            <div className="icon">
-              <BsAwardFill />
-            </div>
-            <div className="text">Best Design Award</div>
-          </div>
-          <div className="highlights verticle">
-            <div className="icon">
-              <FaUser />
-            </div>
-            <div className="text">
-              <span>4K+</span>
-              Happy Customer
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='intro-content'>
+			<div className='layout'>
+				<div className='left-col'>
+					<h1 className='title'>
+						<span className='small-text'>
+							<span className='text'>Hello ✋, I am </span>
+						</span>
+						<span className='big-text'>Pradip Chaudhary</span>
+					</h1>
+					<p>
+						A passionate Full Stack Software Developer 🚀 having a special
+						interest in Frontend technologies and experience of building Web
+						applications with JavaScript / Reactjs / Nodejs and some other
+						libraries and frameworks.
+					</p>
+					<CallToAction text='Contact me' />
+				</div>
+				<div className='right-col'>
+					<img
+						src={girl}
+						alt='Girlimage'
+					/>
+					<div className='highlights horizontal'>
+						<div className='icon'>
+							<BsAwardFill />
+						</div>
+						<div className='text'>Best Design Award</div>
+					</div>
+					<div className='highlights verticle'>
+						<div className='icon'>
+							<FaUser />
+						</div>
+						<div className='text'>
+							<span>4K+</span>
+							Happy Customer
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default IntroContent;
